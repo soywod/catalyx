3ca49d050bf47ab73e9d43d9f00ad71871f31b2b
Clément DOUIN <soywod@users.noreply.github.com>
2020-05-18 20:49:10 +0200

    add basic button

---
+++ src/buttons/button.ts
@@ -0,0 +1,41 @@
+import {parseStyle, parseTemplate, findOrFail} from "../dom-utils";
+import defaultStyle from "./button.css";
+import defaultTpl from "./button.html";
+
+export type ButtonParams = {
+  style?: string;
+  tpl?: string;
+};
+
+export class Button extends HTMLElement {
+  private _button: HTMLButtonElement;
+  private _isToggler: boolean;
+
+  public constructor(params: ButtonParams = {}) {
+    super();
+
+    this.attachShadow({mode: "open", delegatesFocus: true}).append(
+      parseStyle(params.style || defaultStyle),
+      parseTemplate(params.tpl || defaultTpl),
+    );
+
+    this._button = findOrFail(this.shadowRoot, HTMLButtonElement, "button");
+    this._isToggler = this.hasAttribute("toggler");
+  }
+
+  protected connectedCallback() {
+    this.addEventListener("mousedown", this._handleClick);
+  }
+
+  private _handleClick = () => {
+    if (this._isToggler) {
+      if (this.hasAttribute("active")) {
+        this.removeAttribute("active");
+      } else {
+        this.setAttribute("active", "");
+      }
+    }
+  };
+}
+
+customElements.define("cx-button", Button);
